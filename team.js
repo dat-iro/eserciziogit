4c2cd6ad27215c584531620f1a6128a2e130129d
Emiliosang <104137753+Emiliosang@users.noreply.github.com>
2023-08-26 16:51:48 +0200

    Update team.js

--- team.js
+++ team.js
@@ -1,1 +1,16 @@
 const team = [];
+
+const Emilio = {
+    name : "Emilio",
+    surname : "Sanguiné",
+    age : "21",
+    city : "Caltanissetta",
+    hobby : "Music",
+    favoriteFood : "Pizza",
+    favoriteVideoGame : "Elden Ring",
+    favoriteFilm : "Shining",
+    favoriteBook : "Lo Hobbit",
+    petName : " ",
+}
+
+team.push(Emilio)
