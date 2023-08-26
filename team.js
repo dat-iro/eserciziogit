13a37aecb882cec245ca34bec2a8823f9a6b722f
dat-iro <138441382+dat-iro@users.noreply.github.com>
2023-08-26 17:24:26 +0200

    Update team.js

--- team.js
+++ team.js
@@ -1,50 +1,5 @@
 const team = [];
 
-const stefan = {
-    name : "Stefan",
-    surname : "Petrea",
-    age : "21",
-    city : "Palermo",
-    hobby : "Videogames",
-    favoriteFood : "Sushi",
-    favoriteVideogames : "Inazuma Eleven series",
-    favoriteFilm : "Creed",
-    favouriteBook : "Arsene Lupin ladro gentiluomo",
-    petName : "Kelly"
-};
-
-team.push(stefan);
-=======
-const emilio = {
-    name : "Emilio",
-    surname : "Sanguiné",
-    age : "21",
-    city : "Caltanissetta",
-    hobby : "Music",
-    favoriteFood : "Pizza",
-    favoriteVideoGame : "Elden Ring",
-    favoriteFilm : "Shining",
-    favoriteBook : "Lo Hobbit",
-    petName : " ",
-}
-
-team.push(emilio)
-=======
-const giovanni = {
-    name : "Giovanni",
-    surname : "Spatafora",
-    age : 36,
-    city : "Palermo",
-    hobby : "Videomaking",
-    favoriteFood : "Carbonara",
-    favoriteVideogame : "Pro Evolution Soccer",
-    favoriteFilm : "Quarto Potere",
-    favoriteBook : "Sulla strada",
-    petName : ""
-};
-
-team.push(giovanni)
-=======
 const matteo = {
     name : "Matteo",
     surname : "Gatto",
