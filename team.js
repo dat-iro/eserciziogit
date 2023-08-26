f1ba345bfb27e6bd75e9c592cf37ee40a687d1a1
dat-iro <138441382+dat-iro@users.noreply.github.com>
2023-08-26 16:50:55 +0200

    Update team.js

--- team.js
+++ team.js
@@ -1,1 +1,16 @@
 const team = [];
+
+const matteo = {
+    name : "Matteo",
+    surname : "Gatto",
+    age : "23  ",
+    city : "Monterotondo",
+    hobby : "TCG",
+    favoriteFood : "Gelato",
+    favoriteVideoGame : "Pokémon",
+    favoriteFilm : "Into the Wild",
+    favoriteBook : "Siddharta",
+    petName : "Benji"
+};
+
+team.push(matteo);
