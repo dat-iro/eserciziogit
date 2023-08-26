80473d8be9e46a872946487926f1086b113307cb
Stef533 <petreastefaniulian@gmail.com>
2023-08-26 16:51:47 +0200

    Update team.js

---
+++ team.js
@@ -0,0 +1,16 @@
+const team = []
+
+const stefan = {
+    name : "Stefan",
+    surname : "Petrea",
+    age : "21",
+    city : "Palermo",
+    hobby : "Videogames",
+    favoriteFood : "Sushi",
+    favoriteVideogames : "inazuam eleven series",
+    favoriteFilm : "Creed",
+    favouriteBook : "Arsene Lupin ladro gentiluomo",
+    petName : "Kelly"
+};
+
+team.push(Stefan);
