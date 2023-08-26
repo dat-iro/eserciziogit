29965e5951d4292f28322ea8686d082f7e28f51a
giovaspat <141187369+giovaspat@users.noreply.github.com>
2023-08-26 16:51:50 +0200

    Update team.js

--- team.js
+++ team.js
@@ -1,1 +1,18 @@
 const team = [];
+
+const giovanni = {
+
+    name : "Giovanni",
+    surname : "Spatafora",
+    age : 36,
+    city : "Palermo",
+    hobby : "Videomaking",
+    favoriteFood : "Carbonara",
+    favoriteVideogame : "Pro Evolution Soccer",
+    favoriteFilm : "Quarto Potere",
+    favoriteBook : "Sulla strada",
+    petName : ""
+
+};
+
+team.push(giovanni)
