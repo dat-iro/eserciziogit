b296e09af7f98c14683398f0162fda49685e5bd8
giovaspat <141187369+giovaspat@users.noreply.github.com>
2023-08-28 17:28:06 +0200

    added new chef and final consideration

---
+++ newexefile.js
@@ -0,0 +1,10 @@
+let chef1 = "Bruno Barbieri"
+let chef2 = "Antonino Cannavacciuolo"
+let chef3 = "Carlo Cracco"
+
+let consChef1 = chef1 + " è il più simpatico";
+let consChef2 = chef2 + " è il più affamato";
+let consChef3 = chef3 + " è il più altezzoso";
+
+console.log(consChef1 + ", " + consChef2 + ", " + consChef3)
+ 
